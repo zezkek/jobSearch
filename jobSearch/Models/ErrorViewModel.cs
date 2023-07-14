55d7c8f1c1c3f6b2fd946d420aecd3e9bf18eafc
Oleg <super.vec2013@gmail.com>
2023-07-14 18:44:16 +0300

    Добавьте файлы проекта.

---
+++ jobSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace jobSearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
