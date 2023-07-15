0f4d3ab6873bac95c5a2d3e28c4d6fb73bbace0a
Oleg <super.vec2013@gmail.com>
2023-07-15 15:38:37 +0300

    Controller, dbcontext and some pages

---
+++ jobSearch/Controllers/VacancyController.cs
@@ -0,0 +1,22 @@
+﻿using jobSearch.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace jobSearch.Controllers
+{
+    public class VacancyController : Controller
+    {
+
+        private AppDbContext _appDbContext;
+        public VacancyController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult VacancyList()
+        {
+            return View(_appDbContext.Vacancy.ToList());
+        }
+    }
+}
--- jobSearch/Data/AppDbContext.cs
+++ jobSearch/Data/AppDbContext.cs
@@ -7,5 +7,9 @@ namespace jobSearch.Data
     public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Firm> Firms { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Vacancy> Vacancy { get; set; }
     }
 }
